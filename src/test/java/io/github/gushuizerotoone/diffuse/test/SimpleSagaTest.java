45e78ebad8b5b6c83fbe0904036cef0fe58511e5
gushuizerotoone <chengwenzhuce@126.com>
2017-12-18 19:30:56 +0800

    saga redo testcase

--- src/test/java/io/github/gushuizerotoone/diffuse/test/SimpleSagaTest.java
+++ src/test/java/io/github/gushuizerotoone/diffuse/test/SimpleSagaTest.java
@@ -6,7 +6,10 @@ import io.github.gushuizerotoone.diffuse.core.Saga;
 import io.github.gushuizerotoone.diffuse.core.SagaBuilder;
 import io.github.gushuizerotoone.diffuse.core.SagaContext;
 import io.github.gushuizerotoone.diffuse.core.SagaStatus;
+import io.github.gushuizerotoone.diffuse.core.schedule.SagaScheduler;
+import io.github.gushuizerotoone.diffuse.core.schedule.SagaSchedulerImpl;
 import io.github.gushuizerotoone.diffuse.spi.InMemorySagaContextRepo;
+import io.github.gushuizerotoone.diffuse.spi.SagaContextRepo;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -32,10 +35,11 @@ public class SimpleSagaTest {
 
   @Test
   public void testCompensate() {
+    SagaContextRepo sagaContextRepo = new InMemorySagaContextRepo();
     SagaContext sagaContext = new SagaContext("SAGA_ID_2", "mySaga");
     SagaBuilder sb = new SagaBuilder();
     Saga saga = sb.sagaContext(sagaContext)
-            .sagaContextRepository(new InMemorySagaContextRepo())
+            .sagaContextRepository(sagaContextRepo)
             .addService(OrderServiceAdaptor.class)
             .addService(WalletServiceCompensateAdaptor.class) // wallet compensate
             .redoPolicy(CompensateAlwaysPolicy.class)
@@ -44,9 +48,10 @@ public class SimpleSagaTest {
     SagaStatus sagaStatus = saga.process();
     System.out.println(sagaContext);
 
-    saga = sb.rebuild(sagaContext);
-    saga.redo();
+    SagaScheduler sagaScheduler = new SagaSchedulerImpl(sagaContextRepo);
+    sagaScheduler.prepareRedo(sagaContext.getSagaId());
 
-    Assert.assertEquals(SagaStatus.COMPENSATING, sagaStatus);
+    sagaContext = sagaContextRepo.getSagaContext(sagaContext.getSagaId());
+    System.out.println(sagaContext);
   }
 }
